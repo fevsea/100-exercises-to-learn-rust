14d65340b8b1c67fd42b27aa2e4a6e7cd64fa36d
Alejandro Agustin <aagustinb@gmail.com>
2024-11-16 23:17:22 +0100

    Traits done

--- exercises/04_traits/10_assoc_vs_generic/src/lib.rs
+++ exercises/04_traits/10_assoc_vs_generic/src/lib.rs
@@ -13,6 +13,24 @@
 // You don't have to though: it's perfectly okay to write three separate
 // implementations manually. Venture further only if you're curious.
 
+
+trait Power<T> {
+    fn power(self: Self , n: T) -> Self;
+}
+
+impl Power<u16> for u32 {
+    fn power(self, n: u16) -> Self {self.pow(n.into())}
+}
+
+impl Power<u32> for u32 {
+    fn power(self, n: u32) -> Self {self.pow(n.into())}
+}
+
+impl Power<&u32> for u32 {
+    fn power(self, n: &u32) -> Self {self.pow((*n).into())}
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::Power;
--- exercises/04_traits/14_outro/src/lib.rs
+++ exercises/04_traits/14_outro/src/lib.rs
@@ -8,3 +8,83 @@
 //   It should be possible to print its debug representation.
 //
 // Tests are located in the `tests` folder—pay attention to the visibility of your types and methods.
+
+use std::ops::Add;
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub struct SaturatingU16(u16);
+
+impl Add<SaturatingU16> for SaturatingU16 {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        let saturated = self.0.saturating_add(rhs.0);
+        SaturatingU16(saturated)
+    }
+}
+
+impl Add<&SaturatingU16> for SaturatingU16 {
+    type Output = Self;
+
+    fn add(self, rhs: &Self) -> Self::Output {
+        SaturatingU16::add(self, SaturatingU16::from(rhs))
+    }
+}
+
+impl Add<u16> for SaturatingU16 {
+    type Output = Self;
+    fn add(self, rhs: u16) -> Self::Output {
+        SaturatingU16::add(self, SaturatingU16::from(rhs))
+    }
+}
+
+impl Add<&u16> for SaturatingU16 {
+    type Output = Self;
+    fn add(self, rhs: &u16) -> Self::Output {
+        SaturatingU16::add(self, SaturatingU16::from(rhs))
+    }
+}
+
+impl Add<&u8> for SaturatingU16 {
+    type Output = Self;
+    fn add(self, rhs: &u8) -> Self::Output {
+        SaturatingU16::add(self, SaturatingU16::from(rhs))
+    }
+}
+
+impl From<u16> for SaturatingU16 {
+    fn from(s: u16) -> Self {
+        Self(s)
+    }
+}
+
+impl From<u8> for SaturatingU16 {
+    fn from(s: u8) -> Self {
+        Self(s.into())
+    }
+}
+
+impl From<&u16> for SaturatingU16 {
+    fn from(s: &u16) -> Self {
+        Self(*s)
+    }
+}
+
+impl From<&u8> for SaturatingU16 {
+    fn from(s: &u8) -> Self {
+        Self((*s).into())
+    }
+}
+
+
+impl From<&SaturatingU16> for SaturatingU16 {
+    fn from(s: &SaturatingU16) -> Self {
+        (*s).clone()
+    }
+}
+
+impl PartialEq<u16> for SaturatingU16 {
+    fn eq(&self, other: &u16) -> bool {
+        self.0 == *other
+    }
+}
